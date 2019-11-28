fff6908b40b8251fe189624ea1da24a57f6b9441
bae jiun, maybe <alice.maydev@gmail.com>
2019-11-29 03:06:41 +0900

    Update dockerfile and main app

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,3 @@ FROM tiangolo/uwsgi-nginx-flask:python3.7
 COPY ./app /app
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
-
-EXPOSE 80
--- app/main.py
+++ app/main.py
@@ -4,20 +4,14 @@ from flask import Flask
 from controller import Controller
 from view import Router
 
+app = Flask(__name__)
+app.name = 'SSD-demo'
 
-def create():
-    app = Flask(__name__)
-    app.name = 'SSD-demo'
-
+if __name__ == '__main__':
     with open('config.json') as f:
         app.config.update(json.load(f))
 
-    Controller.init(app);
-    Router.init(app);
+    Controller.init(app)
+    Router.init(app)
 
-    return app
-
-
-if __name__ == '__main__':
-    app = create()
     app.run(**app.config['RUN'])
