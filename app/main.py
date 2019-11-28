01663dec93ed7ac509a8b999787ced5fb72b40d5
bae jiun, maybe <alice.maydev@gmail.com>
2019-11-29 04:01:29 +0900

    Update main.py remove main dep

--- app/main.py
+++ app/main.py
@@ -7,11 +7,11 @@ from view import Router
 app = Flask(__name__)
 app.name = 'SSD-demo'
 
+with open('config.json') as f:
+    app.config.update(json.load(f))
+
+Controller.init(app)
+Router.init(app)
+
 if __name__ == '__main__':
-    with open('config.json') as f:
-        app.config.update(json.load(f))
-
-    Controller.init(app)
-    Router.init(app)
-
     app.run(**app.config['RUN'])
